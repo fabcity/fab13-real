c784c70ea33416a6d0a79a0affc67e099237814b
m <m.quintero09@gmail.com>
2017-03-20 17:49:17 +0100

    applications section

--- js/navbar-scrolling.js
+++ js/navbar-scrolling.js
@@ -5,12 +5,14 @@ $(".parallax").on("load resize scroll",function(e){
     if (scroll >= 300) {
        $('#menu').removeClass('navbar-init');
        $('.nav-link').css({ 'color': '#1d3557', 'font-weight': '700' });
+       $('.nav-link:hover').css({ 'color': '#eb5f69' });
        $('.icon-bar').css({ 'border': '1px solid #1d3557' });
        
     } else {
 
        	$('#menu').addClass('navbar-init');
         $('.nav-link').css({ 'color': 'white', 'font-weight': '400' });
+        $('.nav-link:hover').css({ 'color': '#eb5f69', });
         $('.icon-bar').css({ 'border': '1px solid white' });
     }
 
