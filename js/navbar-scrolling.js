3b17b90b22ec7d12352ad14b10aa0d016dea032c
m <m.quintero09@gmail.com>
2017-03-22 17:24:54 +0100

    navbar & twitter fix

--- js/navbar-scrolling.js
+++ js/navbar-scrolling.js
@@ -7,6 +7,9 @@ $(".parallax").on("load resize scroll",function(e){
        $('.nav-link').css({ 'color': '#1d3557', 'font-weight': '700' });
        $('.nav-link:hover').css({ 'color': '#eb5f69' });
        $('.icon-bar').css({ 'border': '1px solid #1d3557' });
+       $('#twitter-band').removeClass('twitter-init');
+       $('.tweet').css({ 'color': '#1d3557'});
+       $('.tweet-link').css({ 'color': '#1d3557'});
        
     } else {
 
@@ -14,6 +17,9 @@ $(".parallax").on("load resize scroll",function(e){
         $('.nav-link').css({ 'color': 'white', 'font-weight': '400' });
         $('.nav-link:hover').css({ 'color': '#eb5f69', });
         $('.icon-bar').css({ 'border': '1px solid white' });
+        $('#twitter-band').addClass('twitter-init');
+        $('.tweet').css({ 'color': '#d8d8d8'});
+        $('.tweet-link').css({ 'color': '#d8d8d8'});
     }
 
 
