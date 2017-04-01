b61851f998fc385b5c788933e92b62da94f5acd2
m <m.quintero09@gmail.com>
2017-04-01 19:48:52 +0200

    map with multiple markers stable version ok

--- js/map.js
+++ js/map.js
@@ -1,233 +1,67 @@
-jQuery(document).ready(function($){
-  //set your google maps parameters
-  var $latitude = -33.449495,
-    $longitude = -70.627864,
-    $map_zoom = 12;
-
-  //google map custom marker icon - .png fallback for IE11
-  var is_internetExplorer11= navigator.userAgent.toLowerCase().indexOf('trident') > -1;
-  var $marker_url = ( is_internetExplorer11 ) ? 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-icon-location.png' : 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-icon-location_1.svg';
-    
-  //define the basic color of your map, plus a value for saturation and brightness
-  var $main_color = '#2d313f',
-    $saturation= -20,
-    $brightness= 5;
-
-  //we define here the style of the map
-  var style= [ 
-    {
-      //set saturation for the labels on the map
-      elementType: "labels",
-      stylers: [
-        {saturation: $saturation}
-      ]
-    },  
-      { //poi stands for point of interest - don't show these lables on the map 
-      featureType: "poi",
-      elementType: "labels",
-      stylers: [
-        {visibility: "off"}
-      ]
-    },
-    {
-      //don't show highways lables on the map
-          featureType: 'road.highway',
-          elementType: 'labels',
-          stylers: [
-              {visibility: "off"}
-          ]
-      }, 
-    {   
-      //don't show local road lables on the map
-      featureType: "road.local", 
-      elementType: "labels.icon", 
-      stylers: [
-        {visibility: "off"} 
-      ] 
-    },
-    { 
-      //don't show arterial road lables on the map
-      featureType: "road.arterial", 
-      elementType: "labels.icon", 
-      stylers: [
-        {visibility: "off"}
-      ] 
-    },
-    {
-      //don't show road lables on the map
-      featureType: "road",
-      elementType: "geometry.stroke",
-      stylers: [
-        {visibility: "off"}
-      ]
-    }, 
-    //style different elements on the map
-    { 
-      featureType: "transit", 
-      elementType: "geometry.fill", 
-      stylers: [
-        { hue: $main_color },
-        { visibility: "on" }, 
-        { lightness: $brightness }, 
-        { saturation: $saturation }
-      ]
-    }, 
-    {
-      featureType: "poi",
-      elementType: "geometry.fill",
-      stylers: [
-        { hue: $main_color },
-        { visibility: "on" }, 
-        { lightness: $brightness }, 
-        { saturation: $saturation }
-      ]
-    },
-    {
-      featureType: "poi.government",
-      elementType: "geometry.fill",
-      stylers: [
-        { hue: $main_color },
-        { visibility: "on" }, 
-        { lightness: $brightness }, 
-        { saturation: $saturation }
-      ]
-    },
-    {
-      featureType: "poi.sport_complex",
-      elementType: "geometry.fill",
-      stylers: [
-        { hue: $main_color },
-        { visibility: "on" }, 
-        { lightness: $brightness }, 
-        { saturation: $saturation }
-      ]
-    },
-    {
-      featureType: "poi.attraction",
-      elementType: "geometry.fill",
-      stylers: [
-        { hue: $main_color },
-        { visibility: "on" }, 
-        { lightness: $brightness }, 
-        { saturation: $saturation }
-      ]
-    },
-    {
-      featureType: "poi.business",
-      elementType: "geometry.fill",
-      stylers: [
-        { hue: $main_color },
-        { visibility: "on" }, 
-        { lightness: $brightness }, 
-        { saturation: $saturation }
-      ]
-    },
-    {
-      featureType: "transit",
-      elementType: "geometry.fill",
-      stylers: [
-        { hue: $main_color },
-        { visibility: "on" }, 
-        { lightness: $brightness }, 
-        { saturation: $saturation }
-      ]
-    },
-    {
-      featureType: "transit.station",
-      elementType: "geometry.fill",
-      stylers: [
-        { hue: $main_color },
-        { visibility: "on" }, 
-        { lightness: $brightness }, 
-        { saturation: $saturation }
-      ]
-    },
-    {
-      featureType: "landscape",
-      stylers: [
-        { hue: $main_color },
-        { visibility: "on" }, 
-        { lightness: $brightness }, 
-        { saturation: $saturation }
-      ]
-      
-    },
-    {
-      featureType: "road",
-      elementType: "geometry.fill",
-      stylers: [
-        { hue: $main_color },
-        { visibility: "on" }, 
-        { lightness: $brightness }, 
-        { saturation: $saturation }
-      ]
-    },
-    {
-      featureType: "road.highway",
-      elementType: "geometry.fill",
-      stylers: [
-        { hue: $main_color },
-        { visibility: "on" }, 
-        { lightness: $brightness }, 
-        { saturation: $saturation }
-      ]
-    }, 
-    {
-      featureType: "water",
-      elementType: "geometry",
-      stylers: [
-        { hue: $main_color },
-        { visibility: "on" }, 
-        { lightness: $brightness }, 
-        { saturation: $saturation }
-      ]
-    }
-  ];
-    
-  //set google map options
-  var map_options = {
-        center: new google.maps.LatLng($latitude, $longitude),
-        zoom: $map_zoom,
-        panControl: false,
-        zoomControl: false,
-        mapTypeControl: false,
-        streetViewControl: false,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        scrollwheel: false,
-        styles: style,
-    }
-    //inizialize the map
-  var map = new google.maps.Map(document.getElementById('google-container'), map_options);
-  //add a custom marker to the map        
-  var marker = new google.maps.Marker({
-      position: new google.maps.LatLng($latitude, $longitude),
-      map: map,
-      visible: true,
-    icon: $marker_url,
-  });
-
-
-
-  //add custom buttons for the zoom-in/zoom-out on the map
-  function CustomZoomControl(controlDiv, map) {
-    //grap the zoom elements from the DOM and insert them in the map 
-      var controlUIzoomIn= document.getElementById('cd-zoom-in'),
-        controlUIzoomOut= document.getElementById('cd-zoom-out');
-      controlDiv.appendChild(controlUIzoomIn);
-      controlDiv.appendChild(controlUIzoomOut);
-
-    // Setup the click event listeners and zoom-in or out according to the clicked element
-    google.maps.event.addDomListener(controlUIzoomIn, 'click', function() {
-        map.setZoom(map.getZoom()+1)
-    });
-    google.maps.event.addDomListener(controlUIzoomOut, 'click', function() {
-        map.setZoom(map.getZoom()-1)
-    });
-  }
-
-  var zoomControlDiv = document.createElement('div');
-  var zoomControl = new CustomZoomControl(zoomControlDiv, map);
-
-    //insert the zoom div on the top left of the map
-    map.controls[google.maps.ControlPosition.LEFT_TOP].push(zoomControlDiv);
+jQuery(function($) {
+    // Asynchronously Load the map API 
+    var script = document.createElement('script');
+    script.src = "//maps.googleapis.com/maps/api/js?key=AIzaSyCSfrpLDLApxgyEAf-zyiHfH5AHOdWj3nU&callback=initialize";
+    document.body.appendChild(script);
 });
+
+function initialize() {
+    var map;
+    var bounds = new google.maps.LatLngBounds();
+    var mapOptions = {
+        mapTypeId: 'roadmap'
+    };
+                    
+    // Display a map on the page
+    map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+    map.setTilt(45);
+        
+    // Multiple Markers
+    var markers = [
+        ['Centro de Extensión Universidad Católica', -33.440701,-70.641212],
+        ['Centro Cultural Gabriela Mistral', -33.438402, -70.638149]
+    ];
+                        
+    // Info Window Content
+    var infoWindowContent = [
+        ['<div class="info_content">' +
+        '<h5>Centro de Extensión Universidad Católica</h5>' +
+        '<p>Fab Conference Venue</p>' +        '</div>'],
+        ['<div class="info_content">' +
+        '<h5>Centro Cultural Gabriela Mistral</h5>' +
+        '<p>Fab Festival Venue</p>' +
+        '</div>']
+    ];
+        
+    // Display multiple markers on a map
+    var infoWindow = new google.maps.InfoWindow(), marker, i;
+    
+    // Loop through our array of markers & place each one on the map  
+    for( i = 0; i < markers.length; i++ ) {
+        var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
+        bounds.extend(position);
+        marker = new google.maps.Marker({
+            position: position,
+            map: map,
+            title: markers[i][0]
+        });
+        
+        // Allow each marker to have an info window    
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                infoWindow.setContent(infoWindowContent[i][0]);
+                infoWindow.open(map, marker);
+            }
+        })(marker, i));
+
+        // Automatically center the map fitting all markers on the screen
+        map.fitBounds(bounds);
+    }
+
+    // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
+    var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
+        this.setZoom(14);
+        google.maps.event.removeListener(boundsListener);
+    });
+    
+}
