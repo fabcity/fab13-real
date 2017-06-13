7f6382274ac4e9556f607d84716dcd7e4944bd8c
mqvlm <m.quintero09@gmail.com>
2017-06-13 15:22:16 +0200

    menu travel dropdown added
    still have to figure out mobile bug

--- js/navbar-scrolling.js
+++ js/navbar-scrolling.js
@@ -15,6 +15,7 @@ function scrollAction(yScroll) {
        $('#menu').removeClass('navbar-init');
        $('.nav-link').css({ 'color': '#1d3557', 'font-weight': '700' });
        $('.nav-link:hover').css({ 'color': '#eb5f69' });
+       $('.nav-link:hover').css({ 'color': '#eb5f69' });
        $('.icon-bar').css({ 'border': '1px solid #1d3557' });
        $('#twitter-band').removeClass('twitter-init');
        $('.tweet').css({ 'color': '#1d3557'});
@@ -25,10 +26,13 @@ function scrollAction(yScroll) {
         $('#menu').addClass('navbar-init');
         $('.nav-link').css({ 'color': 'white', 'font-weight': '400' });
         $('.nav-link:hover').css({ 'color': '#eb5f69', });
+        $('.nav-link:active').css({ 'color': '#eb5f69', });
+        $('.nav-link:focus').css({ 'color': '#eb5f69', });
         $('.icon-bar').css({ 'border': '1px solid white' });
         $('#twitter-band').addClass('twitter-init');
         $('.tweet').css({ 'color': '#d8d8d8'});
         $('.tweet-link').css({ 'color': '#d8d8d8'});
+
     }
 
 }
